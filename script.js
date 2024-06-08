6887cdb4dac2da60fb9f6f8bc3431c4c7b63fa16
vognec <137229162+vognec@users.noreply.github.com>
2024-06-08 09:38:46 -0500

    lock-sroll function to turn off scroll in mobile menu is active

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const body = document.body;
 
 hamburger.addEventListener("click", ()=> {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
+
+// remove/add scroll on body when hamburger or X is clicked
+hamburger.addEventListener("click", ()=> {
+    if (hamburger.classList.contains("active")){
+        body.classList.add("lock-scroll"); 
+    }
+    else {
+        body.classList.remove("lock-scroll");
+    }
+       
+});
+
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+    body.classList.remove("lock-scroll"); // remove body lock scroll when a link is clicked
+}));
+
+
