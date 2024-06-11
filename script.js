bbc42fa70c69cad1fc5fad8be7dc92182c3e0f7e
vognec <137229162+vognec@users.noreply.github.com>
2024-06-10 22:16:24 -0500

    add contact form

--- script.js
+++ script.js
@@ -25,4 +25,33 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     body.classList.remove("lock-scroll"); // remove body lock scroll when a link is clicked
 }));
 
+function validate() {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let error_message = document.getElementById("error-message");
+    
+    error_message.style.padding = "10px"
 
+    let errors = [];
+
+    if (name.length < 2) {
+        errors.push("Please enter a valid name");
+    }
+    if (isNaN(phone) || phone.length != 10) {
+        errors.push("Please enter a valid phone number");
+    }
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        errors.push("Enter a valid email");
+    }
+
+    if (errors.length > 0){
+        error_message.innerHTML = errors.join("<br>");
+        return false;
+    }
+    else {
+        alert("Form Submitted Successfully!");
+        return true;
+    }
+}
